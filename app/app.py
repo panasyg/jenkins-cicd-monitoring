ed106c752100d1eebd55cfa02b6e09f56f6108c3
Ivan <panas2004ivan@gmail.com>
2025-03-17 15:35:00 +0200

    add app/

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify(status='ok'), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
